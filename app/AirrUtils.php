3c50bdeea47e5edbf21a4ed3d7625eccbc84e18d
Bojan Zimonja <bzimonja@sfu.ca>
2019-09-09 11:27:33 -0700

    fixed typo in greater-equal and less-equal filters

--- app/AirrUtils.php
+++ app/AirrUtils.php
@@ -32,7 +32,7 @@ class AirrUtils extends Model
                 && $airr_to_service_array[$content['field']] != '') {
                 $field = $airr_to_service_array[$content['field']];
             } else {
-                return;
+               return;
             }
 
             // check if the field provided exists in the mapping file
@@ -106,13 +106,13 @@ class AirrUtils extends Model
                 }
             case '<=':
                 if ($field != '' && $value != '') {
-                    return '{"' . $field . '":{"$le":' . $value . '}}';
+                    return '{"' . $field . '":{"$lte":' . $value . '}}';
                 } else {
                     return;
                 }
             case '>=':
                 if ($field != '' && $value != '') {
-                    return '{"' . $field . '":{"$ge":' . $value . '}}';
+                    return '{"' . $field . '":{"$gte":' . $value . '}}';
                 } else {
                     return;
                 }
@@ -185,5 +185,143 @@ class AirrUtils extends Model
         } //end switch ($op)
 
         // should not get here
+    }
+    public static function queryOptimizable($query)
+    {
+        //method to check if a rearrangement query can be optimized for iReceptor repository
+        //  returns true if yes, false otherwise
+        //rules are: 
+        //  -if it's an equals query on a single indexed field, or single indexed field and repertoire id, yes
+        //  -if it's a facets query on repertoire_id, and equals on an indexed field, yes         
+        //  -if it's a contains query on junction_aa, and optionally repertoire_id, yes
+        //  -if it's a facets query on repertoire_id with no filter, yes
+        //  -otherwise, not optimizable
+
+        //create helper mappings to avoid hard-coding terms
+        //  TODO? - add 'is_indexed' column to the mapping file, in case we adjust indexes
+
+        try {
+        $airr_names = FileMapping::createMappingArray('service_name', 'airr', ['ir_class'=>['rearrangement', 'ir_rearrangement']]);
+
+        // array of indexed fields - as usual, hard-coded terms are in 'service_name' column of the mapping file
+        //  note that indexed fields on non-AIRR terms can and do exist
+        $indexed_fields = ([$airr_names['ir_project_sample_id'], $airr_names['junction_aa_length'], 
+            $airr_names['junction_aa'], $airr_names['v_call'], $airr_names['d_call'], $airr_names['j_call'], 
+            $airr_names['functional'], $airr_names['ir_annotation_tool']]);
+        $filters = "";
+        $facets = "";
+        if (isset ($query['filters']))
+        {
+            $filters = $query['filters'];
+        }
+
+        if (isset ($query['facets']))
+        {
+            $facets = $query['facets'];
+        }
+        // no filters, no facets - doesn't matter, so go through the regular pipeline
+        if (($filters == "" || sizeof($filters)==0) && $facets == "")
+        {
+            echo "no filter";
+            return false;
+        }
+
+        //first pass is easiest, any facets query not on repertoire_id will not be optimized
+        if ($facets != "" && $facets !="repertoire_id")
+        {
+            echo "bad facet ".$facets;
+            return false;                
+        }
+
+        //if we have no filter, it's a count on repertoire_id and we can definitely optimize it
+        if ($filters=="" || sizeof($filters)==0)
+        {
+            return true;
+        }
+
+        //if filter is not 'and', '=', 'contains' or 'in', we can't do it
+        if (!in_array($filters['op'], ["and", "=", "contains", "in"]))
+        {
+            echo "bad op ".$filters['op'];
+            return false;
+        }
+        //single '=' query on indexed fields, definitely optimizable (if facets exist they should be on repertoire_id at this point 
+        //  so no reason to check)
+        if ($filters['op'] == "=" && in_array($filters['content']['field'], $indexed_fields))
+        {
+            return true;
+        }
+        //Special case - contains query on junction_aa field translates into a 'substring' query and is thus optimizable
+        if ($filters['op'] == "contains" && $filters['content']['field'] == $airr_names['junction_aa'])
+        {
+            return true;
+        }
+
+        //a 'in' query on repertoire_id is optimizable, we just will iterate over it
+        if ($filters['op'] == "in" && $filters['content']['field'] == $airr_names['ir_project_sample_id'])
+        {
+            return true;
+        }
+        //most complicated case is an 'and' filter with two parameters, an indexed field with '=' query and repertoire_id '=' or 'contains'
+        if ($filters['op'] == "and" && is_array($filters["content"]) && sizeof($filters["content"] ==2))
+        {
+            $has_indexed = false;
+            foreach ($filters["content"] as $filter)
+            {
+                //first, check if op is '=', 'in' or 'contains'. Anything else we can't do
+                if ($filter["op"] != '=' && $filter["op"]!== "contains" &$filter["op"]!="in")
+                {
+                    echo "bad op ".$filter["op"];
+                    return false;
+                }
+
+                //can't do anything good if field isn't indexed
+                if (!in_array($filter["content"]["field"], $indexed_fields, true))
+                {
+                    echo "unindex field ".$filter["content"]["field"];
+                    return false;
+                }
+
+                //we can only do 'contains' on junction_aa 
+                if ($filter["op"] == 'contains' && $filter["content"]["field"]!=$airr_names['junction_aa'])
+                {
+                    echo "bad contains ".$filter["content"]["field"];
+                    return false;
+                }
+
+                //'in' only works on repertoire_id
+                if ($filter["op"] == 'in' && $filter["content"]["field"]!=$airr_names['ir_project_sample_id'])
+                {
+                    echo "bad in on ".$filter["content"]["field"];
+                    return false;
+                }
+
+                //'=' works on any indexed field - BUT - we have to make sure query only uses one 
+                //  indexed field and repertoir_id
+                if ($filter["op"] == '=')
+                {
+                    if ($has_indexed)
+                    {
+                        echo ("Attempt to AND multiple fields ".var_dump($filters));
+                        return false;
+                    }
+                    else
+                    {
+                        if (in_array($filter["content"]["field"], $indexed_fields) && $filter["content"]["field"]!=$airr_names['ir_project_sample_id'])
+                        {
+                            $has_indexed = true;
+                        }
+                    }
+                }            
+            }
+            return true;
+        }
+        // shouldn't get here
+        echo "no return"; die();
+        return false;     
+        }
+        catch (\Exception $e){
+            return false;
+        } 
     }
 }
