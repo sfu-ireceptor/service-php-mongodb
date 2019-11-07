311c3695511848c3a26a34c3ecd38009f8c0ff1a
Jerome Jaglale <jerome.jaglale@gmail.com>
2019-11-07 23:15:09 +0000

    Apply fixes from StyleCI

--- app/Sample.php
+++ app/Sample.php
@@ -102,7 +102,7 @@ class Sample extends Model
         $db_names = FileMapping::createMappingArray('service_name', 'ir_mongo_database', ['ir_class'=>['repertoire', 'ir_repertoire']]);
         $airr_names = FileMapping::createMappingArray('service_name', 'airr', ['ir_class'=>['repertoire', 'ir_repertoire']]);
         $repository_to_airr = FileMapping::createMappingArray('ir_mongo_database', 'airr', ['ir_class'=>['repertoire', 'ir_repertoire']]);
-        $db_names_to_airr_types = FileMapping::createMappingArray('ir_mongo_database', 'airr_type',['ir_class'=>['repertoire', 'ir_repertoire']]);
+        $db_names_to_airr_types = FileMapping::createMappingArray('ir_mongo_database', 'airr_type', ['ir_class'=>['repertoire', 'ir_repertoire']]);
 
         //each iReceptor 'sample' is an AIRR repertoire consisting of a single sample and  a single rearrangement set
         //  associated with it, so we will take the array of samples and place each element into an appropriate section
@@ -126,22 +126,21 @@ class Sample extends Model
 
                     // typecast the return values
                     $return_value = $return_element;
-                    if (isset($db_names_to_airr_types[$return_key]))
-                    {
+                    if (isset($db_names_to_airr_types[$return_key])) {
                         switch ($db_names_to_airr_types[$return_key]) {
                             // make sure that type actually matches value or fail
                             case 'integer':
                                 if (is_array($return_element)) {
-                                    $return_value =  array_map('intval',$return_element);
+                                    $return_value = array_map('intval', $return_element);
                                 } else {
-                                     $return_value = (int) $return_element;
+                                    $return_value = (int) $return_element;
                                 }
                                 break;
                             case 'number':
                                 if (is_array($return_element)) {
-                                    $return_value =  array_map('floatval',$return_element);
+                                    $return_value = array_map('floatval', $return_element);
                                 } else {
-                                     $return_value = (float) $return_element;
+                                    $return_value = (float) $return_element;
                                 }
                                 break;
                             case 'boolean':
