2ce07d1968eb76cb503a4b7a68114f40f91c8bd1
Jerome Jaglale <jerome_jaglale@sfu.ca>
2019-11-21 14:34:16 -0800

    Add DB seeder for samples collection

---
+++ database/seeds/SamplesCollectionSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SamplesCollectionSeeder extends Seeder
+{
+    public function run()
+    {
+    	// delete existing collection
+    	DB::table('samples')->delete();
+
+    	// create/seed collection
+    	$json = File::get('database/seeds/samples.json');
+    	$data = json_decode($json);
+    	foreach ($data as $obj) {
+			DB::collection('samples')->insert($data);
+    	}
+    }
+}
