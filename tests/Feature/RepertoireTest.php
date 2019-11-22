933efbb717cdbbe8373f1406f7e45cec5d610e0b
Jerome Jaglale <jerome.jaglale@gmail.com>
2019-11-22 23:38:30 +0000

    Apply fixes from StyleCI

--- tests/Feature/RepertoireTest.php
+++ tests/Feature/RepertoireTest.php
@@ -2,33 +2,34 @@
 
 namespace Tests\Feature;
 
-use Tests\TestCase;
 use SamplesCollectionSeeder;
+use Tests\TestCase;
 
 class RepertoireTest extends TestCase
 {
-	public function postJsonString($uri, $content)
-	{
+    public function postJsonString($uri, $content)
+    {
         $headers = [
             'CONTENT_LENGTH' => mb_strlen($content, '8bit'),
             'CONTENT_TYPE' => 'application/json',
             'Accept' => 'application/json',
         ];
 
-		$response = $this->call('POST', $uri, [], [], [], $this->transformHeadersToServerVars($headers), $content);
-		return $response;
-	}
+        $response = $this->call('POST', $uri, [], [], [], $this->transformHeadersToServerVars($headers), $content);
 
-	public function setUp()
-	{
-	    parent::setUp();
-		$this->seed(SamplesCollectionSeeder::class);
-	}
+        return $response;
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->seed(SamplesCollectionSeeder::class);
+    }
 
     /** @test */
     public function valid_JSON()
-    {        
-    	$response = $this->postJson('/airr/v1/repertoire');
+    {
+        $response = $this->postJson('/airr/v1/repertoire');
         $response->assertStatus(200);
 
         $response->assertJson(['Info' => []]);
@@ -38,9 +39,9 @@ class RepertoireTest extends TestCase
     /** @test */
     public function sex_filter()
     {
-    	$s = '{"filters": {"op": "=","content":{"field": "subject.sex","value": "Female"}}}';
+        $s = '{"filters": {"op": "=","content":{"field": "subject.sex","value": "Female"}}}';
 
-    	$response = $this->postJsonString('/airr/v1/repertoire', $s);
+        $response = $this->postJsonString('/airr/v1/repertoire', $s);
         $response->assertStatus(200);
 
         $response->assertJson(['Info' => []]);
