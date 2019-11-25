8468b19adaeeb33144e3db18f2212ba6ce75633d
Jerome Jaglale <jerome_jaglale@sfu.ca>
2019-11-25 13:31:43 -0800

    Improve test for sex filter

--- tests/Feature/RepertoireTest.php
+++ tests/Feature/RepertoireTest.php
@@ -32,19 +32,56 @@ class RepertoireTest extends TestCase
         $response = $this->postJson('/airr/v1/repertoire');
         $response->assertStatus(200);
 
-        $response->assertJson(['Info' => []]);
-        $response->assertJson(['Repertoire' => []]);
+        $response->assertJson([]);
     }
 
     /** @test */
-    public function sex_filter()
+    public function correct_JSON()
     {
-        $s = '{"filters": {"op": "=","content":{"field": "subject.sex","value": "Female"}}}';
-
-        $response = $this->postJsonString('/airr/v1/repertoire', $s);
-        $response->assertStatus(200);
-
+        $response = $this->postJson('/airr/v1/repertoire');
+        
+        // has info and repertoire objects
         $response->assertJson(['Info' => []]);
         $response->assertJson(['Repertoire' => []]);
+
+        $json = $response->content();
+        $t = json_decode($json);
+
+        // has exactly 2 samples
+        $this->assertCount(2, $t->Repertoire);
+    }
+
+    /** @test */
+    public function sex_filter_female()
+    {
+        $s = '{"filters": {"op": "=","content":{"field": "subject.sex","value": "Female"}}}';
+        $response = $this->postJsonString('/airr/v1/repertoire', $s);
+
+        $json = $response->content();
+        $t = json_decode($json);
+
+        // has exactly 1 sample
+        $this->assertCount(1, $t->Repertoire);
+
+        // female sample
+        $sex = data_get($t, 'Repertoire.0.subject.sex');
+        $this->assertEquals($sex, 'Female');
+    }
+
+    /** @test */
+    public function sex_filter_male()
+    {
+        $s = '{"filters": {"op": "=","content":{"field": "subject.sex","value": "Male"}}}';
+        $response = $this->postJsonString('/airr/v1/repertoire', $s);
+
+        $json = $response->content();
+        $t = json_decode($json);
+
+        // has exactly 1 sample
+        $this->assertCount(1, $t->Repertoire);
+
+        // male sample
+        $sex = data_get($t, 'Repertoire.0.subject.sex');
+        $this->assertEquals($sex, 'Male');
     }
 }
