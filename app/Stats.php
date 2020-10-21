6551ea85f05b1e0501b62f2127bd3889a110d1d1
bzimonja <30536574+bzimonja@users.noreply.github.com>
2020-10-21 10:51:18 -0700

    remove an obsolete method, add 'repertoire' element to list of repertoires in input

--- app/Stats.php
+++ app/Stats.php
@@ -22,80 +22,6 @@ class Stats extends Model
         return $this->collection;
     }
 
-    public static function rearrangementCountRequest($params)
-    {
-        // given an array of repertoires, find the count of rearrangements for each
-        //  -as an optimization, iReceptor stores rearrangement counts in repertoire collection
-        //   for each set of rearrangement files, identified by the field mapped by
-        //   service_name field ir_project_sample_id (default is repertoire_id but could be others)
-        //  -for each repertoire id given, we can find all the repertoire entires and extract counts.
-        //   This is faster than doing a search on stats
-
-        $repertoire_service_to_db_mapping = FileMapping::createMappingArray('service_name', 'ir_repository', ['ir_class'=>['repertoire', 'ir_repertoire', 'Repertoire', 'IR_Repertoire']]);
-        $repertoire_db_types = FileMapping::createMappingArray('ir_repository', 'ir_repository_type', ['ir_class'=>['rearrangement', 'ir_rearrangement', 'Repertoire', 'IR_Repertoire']]);
-
-        $sample_id_list = [];
-        $sample_id_list['total'] = 0;
-        $sample_id_list['counts'] = [];
-        // if 'repertoires' is set, loop through it and create an array of repertoire ids to search
-        if (isset($params['repertoires'])) {
-            if (! is_array($params['repertoires'])) {
-                return 'error';
-            }
-
-            $repertoire_id_list = [];
-
-            foreach ($params['repertoires'] as $repertoire_object) {
-                //each repertoire object has repertoire_id, optional sample_processing_id and
-                //  optional data_processing id, that we can use to find the repertoire in repertoire collection
-                if (isset($repertoire_object['repertoire_id']) && ! is_null($repertoire_object['repertoire_id'])) {
-                    $repertoire_id = AirrUtils::typeConvertHelperRaw($repertoire_object['repertoire_id'], $repertoire_db_types[$repertoire_service_to_db_mapping['ir_project_sample_id']]);
-                    $repertoire_query[$repertoire_service_to_db_mapping['ir_project_sample_id']] = $repertoire_id;
-                } else {
-                    //repertoire id is required, fail if it's not provided
-                    return 'error';
-                }
-                if (isset($repertoire_object['sample_processing_id'])) {
-                    //we have sample_processing id, so return object should have it
-                    $return_sample_processing = true;
-                    if (! is_null($repertoire_object['sample_processing_id']) && $repertoire_object['sample_processing_id'] != 'all') {
-                        $sample_processing_id = AirrUtils::typeConvertHelperRaw($repertoire_object['sample_processing_id'], $repertoire_db_types[$repertoire_service_to_db_mapping['sample_processing_id']]);
-                        $repertoire_query[$repertoire_service_to_db_mapping['sample_processing_id']] = $sample_processing_id;
-                    }
-                }
-                if (isset($repertoire_object['data_processing_id'])) {
-                    //we have data_processing id, so return object should have it
-                    $return_data_processing = true;
-                    if (! is_null($repertoire_object['data_processing_id']) && $repertoire_object['data_processing_id'] != 'all') {
-                        $data_processing_id = AirrUtils::typeConvertHelperRaw($repertoire_object['data_processing_id'], $repertoire_db_types[$repertoire_service_to_db_mapping['data_processing_id']]);
-                        $repertoire_query[$repertoire_service_to_db_mapping['data_processing_id']] = $data_processing_id;
-                    }
-                }
-                $repertoire_result = AirrRepertoire::findRepertoire($repertoire_query);
-                foreach ($repertoire_result as $result) {
-                    $sample_id_list['total'] += $result[$repertoire_service_to_db_mapping['ir_sequence_count']];
-                    $return_object = [];
-                    $return_object['repertoire_id'] = $result [$repertoire_service_to_db_mapping['ir_project_sample_id']];
-                    if (isset($return_sample_processing)) {
-                        $return_object['sample_processing_id'] = $result [$repertoire_service_to_db_mapping['sample_processing_id']];
-                    }
-                    if (isset($return_data_processing)) {
-                        $return_object['data_processing_id'] = $result [$repertoire_service_to_db_mapping['data_processing_id']];
-                    }
-                    $return_object['count'] = $result[$repertoire_service_to_db_mapping['ir_sequence_count']];
-                    $sample_id_list['counts'][] = $return_object;
-                }
-            }
-        }
-
-        return $sample_id_list;
-    }
-
-    public static function rearrangementCountResponse($count_list)
-    {
-        return json_encode($count_list, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
-    }
-
     public static function statsRequest($params, $entry_point)
     {
         // given an array of repertoires, find the count of junction lengts for each
@@ -152,16 +78,24 @@ class Stats extends Model
             }
 
             $repertoire_id_list = [];
-            foreach ($params[$service_to_api_input_mapping['repertoires']] as $repertoire_object) {
+            foreach ($params[$service_to_api_input_mapping['repertoires']] as $repertoire_element) {
                 //each repertoire object has repertoire_id, optional sample_processing_id and
                 //  optional data_processing id, that we can use to find the repertoire in repertoire collection
+                if (isset($repertoire_element[$service_to_api_input_mapping['repertoire']]) )
+                {
+                    $repertoire_object = $repertoire_element[$service_to_api_input_mapping['repertoire']];
+                }
+                else
+                {
+                    return 'error';
+                }
                 if (isset($repertoire_object['repertoire_id']) && ! is_null($repertoire_object['repertoire_id'])) {
                     $repertoire_id = AirrUtils::typeConvertHelperRaw($repertoire_object['repertoire_id'], $repertoire_db_types[$repertoire_service_to_db_mapping['ir_project_sample_id']]);
                     $repertoire_query[$repertoire_service_to_db_mapping['ir_project_sample_id']] = $repertoire_id;
                 } else {
                     //repertoire id is required, fail if it's not provided
                     return 'error';
-                }
+                }                
                 if (isset($repertoire_object['sample_processing_id'])) {
                     //we have sample_processing id, so return object should have it
                     $return_sample_processing = true;
