e29cccf8adf5c681bdb6a5f84a2bafbbbc7ffe23
bzimonja <30536574+bzimonja@users.noreply.github.com>
2020-10-26 13:56:40 -0700

    allow an empty JSON file on repertoire entry point

--- app/Http/Controllers/AirrApiController.php
+++ app/Http/Controllers/AirrApiController.php
@@ -38,9 +38,13 @@ class AirrApiController extends Controller
     {
         // /repertoire entry point that resolves an AIRR API repertoire query request and
         //    currently returns an iReceptor API response
+        if (sizeof($request->all()) == 0) {
+            $params = json_encode('{}');
+        } else {
+            $params = $request->json()->all();
+        }
         $params = $request->json()->all();
         $response = [];
-        //if (! isset($params) ) {
         $error = json_last_error();
         if ($error) {
             //something went bad and Laravel cound't parse the parameters as JSON
