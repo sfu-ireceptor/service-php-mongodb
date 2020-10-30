f9d124db45f60c5a23c02fe4a727267392624d85
bzimonja <30536574+bzimonja@users.noreply.github.com>
2020-10-30 14:12:00 -0700

    fix escaping on the '/' character in filters, add un-enscaped parameter to JSON responses

--- app/AirrUtils.php
+++ app/AirrUtils.php
@@ -42,9 +42,9 @@ class AirrUtils extends Model
                 break;
             case 'boolean':
                 if (is_array($value)) {
-                    return json_encode(array_map('boolval', $value));
+                    return json_encode(array_map('boolval', $value), JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES );
                 } else {
-                    return json_encode(boolval($value));
+                    return json_encode(boolval($value), JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES );
                 }
                 break;
             default:
@@ -324,7 +324,7 @@ class AirrUtils extends Model
                 break;
             case 'contains':
                 if (isset($field) && $field != '' && isset($value) && $type == 'string') {
-                    return '{"' . $field . '":{"$regex":' . preg_quote($value) . ',"$options":"i"}}';
+                    return '{"' . $field . '":{"$regex":' . preg_quote($value, '/') . ',"$options":"i"}}';
                 } else {
                     return;
                 }
