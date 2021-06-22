a59ea4d8a314bdb6f1fbaf5958bc6da503d91dbc
bzimonja <30536574+bzimonja@users.noreply.github.com>
2021-06-21 19:47:19 -0700

    skip unknown fields in TSV, upper-case 'contains' queries on junction_aa when optimized

--- app/AirrUtils.php
+++ app/AirrUtils.php
@@ -598,9 +598,9 @@ class AirrUtils extends Model
                         $sample_id_list[] = self::typeConvertHelperRaw($filter_piece['content']['value'], $db_types_array[$filter_piece['content']['field']]);
                     }
                 } else {
-                    // if we have junction_aa, we do a query on substring field instead
+                    // if we have junction_aa, we do a query on substring field instead, case insensitive
                     if ($airr_to_repository_mapping[$filter_piece['content']['field']] == $service_to_airr_mapping['junction_aa']) {
-                        $db_filters[$service_to_db_mapping['substring']] = (string) $filter_piece['content']['value'];
+                        $db_filters[$service_to_db_mapping['substring']] = strtoupper((string) $filter_piece['content']['value']);
                     } else {
                         $db_filters[$airr_to_repository_mapping[$filter_piece['content']['field']]] = self::typeConvertHelperRaw($filter_piece['content']['value'], $db_types_array[$filter_piece['content']['field']]);
                     }
@@ -619,7 +619,7 @@ class AirrUtils extends Model
             } else {
                 // if we have junction_aa, we do a query on substring field instead
                 if ($airr_to_repository_mapping[$filter['content']['field']] == $service_to_airr_mapping['junction_aa']) {
-                    $db_filters[$service_to_db_mapping['substring']] = (string) $filter['content']['value'];
+                    $db_filters[$service_to_db_mapping['substring']] = strtoupper((string) $filter['content']['value']);
                 } else {
                     $db_filters[$airr_to_repository_mapping[$filter['content']['field']]] = self::typeConvertHelperRaw($filter['content']['value'], $db_types_array[$filter['content']['field']]);
                 }
