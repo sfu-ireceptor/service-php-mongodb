c7d3e561873bc1c20516a7f52ed6494a65fbbbab
jeromejaglale <jerome.jaglale@gmail.com>
2022-09-09 00:05:47 +0000

    Improve appearance of default home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
 
 		<!-- css -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-		<link href="/css/main.css?v=1" rel="stylesheet">
+		<link href="/css/main.css?v=2" rel="stylesheet">
 
 		<!-- IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
