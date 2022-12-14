4304a0aad4fad3e1c42f5cf5dcdf4b0c2da9900d
Ubuntu <bzimonja@sfu.ca>
2022-12-14 22:24:29 +0000

    first batch of changes for PHP8 compatiblity

---
+++ database/seeders/SamplesCollectionSeeder.php
@@ -0,0 +1,21 @@
+<?php
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;
+
+class SamplesCollectionSeeder extends Seeder
+{
+    public function run()
+    {
+        // delete existing collection
+        DB::table('samples')->delete();
+
+        // get data
+        $json = File::get('database/seeds/samples.json');
+        $data = json_decode($json, true);
+
+        foreach ($data as $t) {
+            // add to collection
+            DB::collection('samples')->insert($t);
+        }
+    }
+}
